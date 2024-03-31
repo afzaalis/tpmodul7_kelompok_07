bb2b50595c1b6424f2d24422a9f135223a0b37ba
Afzaal Isnaufal <70215760+afzaalis@users.noreply.github.com>
2024-03-31 23:04:40 +0700

    menambahkan class DataMahasiswa_1302220104

--- tpmodul7_kelompok_07/Program.cs
+++ tpmodul7_kelompok_07/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul7_kelompok_07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa_1302220104_PRAKTIKAN mhs = new DataMahasiswa_1302220104_PRAKTIKAN();
+
+        mhs.ReadJSON(); mhs = new DataMahasiswa_1302220104_PRAKTIKAN();
+
+        mhs.ReadJSON();
+    }
+}
