962912555fca20e8470d0d32130825f736e16dd5
Afzaal Isnaufal <70215760+afzaalis@users.noreply.github.com>
2024-03-31 23:58:24 +0700

    menambahkan class KuliahMahasiswa

--- tpmodul7_kelompok_07/Program.cs
+++ tpmodul7_kelompok_07/Program.cs
@@ -5,9 +5,10 @@ class Program
     static void Main(string[] args)
     {
         DataMahasiswa_1302220104_PRAKTIKAN mhs = new DataMahasiswa_1302220104_PRAKTIKAN();
-
-        mhs.ReadJSON(); mhs = new DataMahasiswa_1302220104_PRAKTIKAN();
-
         mhs.ReadJSON();
+
+        KuliahMahasiswa_1302220104 kuliahMahasiswa = new KuliahMahasiswa_1302220104();
+        kuliahMahasiswa.ReadJson2(); 
+    
     }
 }
